16c62e7df2502dac7871269091365a1d33988a13
Don Phillips <dphillips@prosper.com>
2019-12-09 12:25:34 -0800

    Double quotes make all the difference

--- step.sh
+++ step.sh
@@ -3,7 +3,7 @@ set -ex
 
 echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
 
-git config --global user.name ${git_user_name}
-git config --global user.email ${git_email_address}
+git config --global user.name "${git_user_name}"
+git config --global user.email "${git_email_address}"
 
 exit 0
