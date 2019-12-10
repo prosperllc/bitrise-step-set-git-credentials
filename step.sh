74057b45fcebd6bfc754bf8e60ba8b4500808bc1
Don Phillips <dphillips@prosper.com>
2019-12-10 10:10:08 -0800

    Delete echo statement from template

--- step.sh
+++ step.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
-
 git config --global user.name "${git_user_name}"
 git config --global user.email "${git_email_address}"
 
